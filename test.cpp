fc97af2362a340c95b6d1c395641e01e7789788a
FTD <zxc2012@gmail.com>
2014-07-27 11:55:11 +0800

    Temp.

--- test.cpp
+++ test.cpp
@@ -5,11 +5,31 @@
 using namespace std;
 using namespace mmtraining;
 
+class MyWork : public Work
+{
+public:
+	bool NeedDelete() const
+	{
+		return false;	
+	}
+
+	int DoWork()
+	{
+		printf("MyWork is running.\n");	
+		//cout << "MyWork is running." << endl;
+		return 0;
+	}
+};
+
 int main()
 {
-	Thread threadType;
-	ThreadPool tp;
-	tp.Start(10, threadType);
-	tp.JoinAll();
+	MyWork myWork;
+	WorkQueue wq;
+	WorkerThreadPool whp;
+	for (int i=0; i<50; i++)
+		whp.AddWork(&myWork);
+	whp.Start(10);
+	//whp.JoinAll();
+	//whp.Shutdown();
 	return 0;
 }
--- threadpool.cpp
+++ threadpool.cpp
@@ -5,179 +5,223 @@
 
 namespace mmtraining {
 
-/////////////////////////////////////////////////Thread
+	/////////////////////////////////////////////////Thread
 
 
-Thread::Thread() : running(false), target(NULL), tid(-1) {}
+	Thread::Thread() : running(false), target(NULL), tid(-1) {}
 
-Thread::Thread(Runnable& t) : running(false), target(&t), tid(-1) {}
+	Thread::Thread(Runnable& t) : running(false), target(&t), tid(-1) {}
 
-Thread::~Thread() {
-    // TODO: 释放资源
-	target = NULL;
-	running = false;
-}
-
-int Thread::Start() {
-    // TODO: 启动线程, 运行 Run
-	if ((pthread_create(&tid, NULL, start_thread, (void*)this)) != 0)
-		return -1;
-	else
-		return 0;
-}
-
-pthread_t Thread::GetId() const {
-    // TODO:获取线程id 
-	if ((int)tid != -1)	
-		return tid;
-	else
-	    return -1;
-}
-
-int Thread::Run() {
-    running = true;
-    // 处理逻辑
-    int ret = -1;
-	
-    if (target != NULL) { // 若指定了target, 则运行target逻辑
-        ret = target->Run();
-    } else { // 否则运行 DoRun 逻辑
-        ret = this->DoRun();
-    }
-    running = false;
-    return ret;
-}
-
-int Thread::DoRun() {
-	//printf("Thread %d is running in the class Thread.\n", (int)pthread_self());
-    return 0;
-}
-
-bool Thread::IsRunning() const {
-    return running;
-}
-
-int Thread::Join() {
-    // TODO: 完成代码
-	if (pthread_join(tid, NULL) == 0)
-	{
-		return 0;
+	Thread::~Thread() {
+		// TODO: 释放资源
 	}
-	else
-	    return -1;
-}
 
-void* Thread::start_thread(void *arg)
-{
-	Thread *ptr = (Thread*)arg;
-	//sleep(0.01);
-	ptr->Run();
-	return NULL;
-}
+	int Thread::Start() {
+		// TODO: 启动线程, 运行 Run
+		if ((pthread_create(&tid, NULL, start_thread, (void*)this)) != 0)
+			return -1;
+		else
+			return 0;
+	}
 
-/////////////////////////////////////////////////ThreadPool
-
-ThreadPool::ThreadPool()
-{
-}
-
-ThreadPool::~ThreadPool() {
-    // TODO: 完成代码
-}
-
-int ThreadPool::Start(int threadCount, Runnable& target) {
-    // TODO: 完成代码
-	for (int i=0; i<threadCount; i++)
-	{
-		Thread *newOneThread= new Thread(target);
-		threads.push_back(newOneThread);
-		if (newOneThread->Start() != 0)
+	pthread_t Thread::GetId() const {
+		// TODO:获取线程id 
+		if ((int)tid != -1)	
+			return tid;
+		else
 			return -1;
 	}
-    return 0;
-}
 
-int ThreadPool::JoinAll() {
-    // TODO: 完成代码
-	for (Thread* &oneThread : threads)
-	{
-		if (oneThread->Join() != 0)
-			return -1;	
+	int Thread::Run() {
+		running = true;
+		// 处理逻辑
+		int ret = -1;
+
+		if (target != NULL) { // 若指定了target, 则运行target逻辑
+			ret = target->Run();
+		} else { // 否则运行 DoRun 逻辑
+			ret = this->DoRun();
+		}
+		running = false;
+		return ret;
 	}
-    return 0;
-}
 
-///////////////////////////////////////////////WorkQueue
+	int Thread::DoRun() {
+		printf("Thread %lld is running with no job in the class Thread.\n", (long long)pthread_self());
+		return 0;
+	}
 
-WorkQueue::WorkQueue() : shutdown(false) {
-    // TODO: 初始化
-}
+	bool Thread::IsRunning() const {
+		return running;
+	}
 
-WorkQueue::~WorkQueue() {
-    // TODO: 释放资源
-}
+	int Thread::Join() {
+		// TODO: 完成代码
+		if (pthread_join(tid, NULL) == 0)
+		{
+			return 0;
+		}
+		else
+			return -1;
+	}
 
-int WorkQueue::AddWork(Work* work) {
-    // TODO: 完成代码
-	works.push_back(work);
-	works.pop_front();
-    return -1;
-}
+	void* Thread::start_thread(void *arg)
+	{
+		Thread *ptr = (Thread*)arg;
+		//sleep(0.01);
+		ptr->Run();
+		return NULL;
+	}
 
-Work* WorkQueue::GetWork() {
-    // TODO: 完成代码
-    return NULL;
-}
+	/////////////////////////////////////////////////ThreadPool
 
-int WorkQueue::Shutdown() {
-    // TODO: 完成代码
-    return -1;
-}
+	ThreadPool::ThreadPool()
+	{
+	}
 
-bool WorkQueue::IsShutdown() {
-    return shutdown;
-}
+	ThreadPool::~ThreadPool() {
+		// TODO: 完成代码
+	}
 
-/////////////////////////////////////////////////Worker
+	int ThreadPool::Start(int threadCount, Runnable& target) {
+		// TODO: 完成代码
+		for (int i=0; i<threadCount; i++)
+		{
+			Thread *newOneThread= new Thread(target);
+			threads.push_back(newOneThread);
+			if (newOneThread->Start() != 0)
+				return -1;
+		}
+		return 0;
+	}
 
-Worker::Worker(WorkQueue& queue) : workQueue(queue) {}
+	int ThreadPool::JoinAll() {
+		// TODO: 完成代码
+		for (Thread* &oneThread : threads)
+		{
+			if (oneThread->Join() != 0)
+				return -1;	
+		}
+		return 0;
+	}
 
-Worker::~Worker() {
-    // TODO: 释放资源
-}
+	///////////////////////////////////////////////WorkQueue
 
-int Worker::Run() {
-    // TODO: 工作循环
-    return -1;
-}
+	WorkQueue::WorkQueue() : shutdown(false) {
+		// TODO: 初始化
+		if (pthread_mutex_init(&mutex, NULL) != 0)
+		{
+			printf("The pthread_mutex_init failed.");	
+			exit(-1);
+		}
+		if (pthread_cond_init(&cond, NULL) != 0)
+		{
+			printf("The pthread_cond_init failed.");	
+			exit(-1);
+		}
+	}
 
-/////////////////////////////////////////////////WorkerThreadPool
+	WorkQueue::~WorkQueue() {
+		// TODO: 释放资源
+	}
 
-WorkerThreadPool::WorkerThreadPool() : worker(workQueue) {}
+	int WorkQueue::AddWork(Work* work) {
+		// TODO: 完成代码
+		works.push_back(work);
+		pthread_cond_signal(&cond);
+		return 0;
+	}
 
-WorkerThreadPool::~WorkerThreadPool() {
-    // TODO: 完成代码
-}
+	Work* WorkQueue::GetWork() {
+		// TODO: 完成代码
+		Work* oneWork = NULL; 
+		pthread_mutex_lock(&mutex);
+		if (!shutdown && works.empty())
+			pthread_cond_wait(&cond, &mutex);
+		else
+		{
+			oneWork = works.front();
+			works.pop_front();
+		}
+		pthread_mutex_unlock(&mutex);
+		if (oneWork != NULL)
+			return (Work*)oneWork;
+		else
+			return NULL;
+	}
 
-int WorkerThreadPool::Start(int threadCount) {
-    return pool.Start(threadCount, worker);
-}
+	int WorkQueue::Shutdown() {
+		// TODO: 完成代码
+		printf("Shutdown Function works.\n");
+		while (!works.empty())	
+		{
+			pthread_cond_signal(&cond);
+		}
+		if (works.empty())
+		{
+			shutdown = true;
+			pthread_cond_destroy(&cond);
+			return 0;
+		}
+		else
+			return -1;
+	}
 
-int WorkerThreadPool::Start(int threadCount, Runnable& target) {
-    return pool.Start(threadCount, target);
-}
+	bool WorkQueue::IsShutdown() {
+		return shutdown;
+	}
 
-int WorkerThreadPool::AddWork(Work* work) {
-    return workQueue.AddWork(work);
-}
+	/////////////////////////////////////////////////Worker
 
-int WorkerThreadPool::Shutdown() {
-    return workQueue.Shutdown();
-}
+	Worker::Worker(WorkQueue& queue) : workQueue(queue) {}
 
-int WorkerThreadPool::JoinAll() {
-    return pool.JoinAll();
-}
+	Worker::~Worker() {
+		// TODO: 释放资源
+	}
+
+	int Worker::Run() {
+		// TODO: 工作循环
+		printf("Status of Shutdown is %d.\n", workQueue.IsShutdown());
+		printf("Thread %lld do this.\n", (long long)pthread_self());
+		while (!workQueue.IsShutdown())
+		{
+			//printf("Worker is working.\n");
+			Work* oneWork = workQueue.GetWork();
+			if (!oneWork->NeedDelete() && oneWork->DoWork() != 0)
+			{
+				return -1;	
+			}
+		}
+		return 0;
+	}
+
+	/////////////////////////////////////////////////WorkerThreadPool
+
+	WorkerThreadPool::WorkerThreadPool() : worker(workQueue) {}
+
+	WorkerThreadPool::~WorkerThreadPool() {
+		// TODO: 完成代码
+	}
+
+	int WorkerThreadPool::Start(int threadCount) {
+		return pool.Start(threadCount, worker);
+	}
+
+	int WorkerThreadPool::Start(int threadCount, Runnable& target) {
+		return pool.Start(threadCount, target);
+	}
+
+	int WorkerThreadPool::AddWork(Work* work) {
+		return workQueue.AddWork(work);
+	}
+
+	int WorkerThreadPool::Shutdown() {
+		return workQueue.Shutdown();
+	}
+
+	int WorkerThreadPool::JoinAll() {
+		return pool.JoinAll();
+	}
 
 } // mmtraining
 
