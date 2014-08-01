d19f9ba0a702d3305a221a2cc36b4c5e23a52de6
FTD <zxc2012@gmail.com>
2014-08-01 11:29:39 +0800

    Add test file.

---
+++ test/test.cpp
@@ -0,0 +1,44 @@
+#include <fcntl.h>
+#include <iostream>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <cstdio>
+#include <sys/wait.h>
+#include <cstring>
+
+using namespace std;
+
+class People
+{
+public:
+	string name;
+	int age;
+};
+int main(int argc, char* argv[])
+{
+	int fd, i;
+	People *p_map;
+	char temp;
+
+	fd = open(argv[1], O_CREAT|O_RDWR|O_TRUNC, 00777);
+	lseek(fd, sizeof(People)*5-1, SEEK_SET);
+	write(fd, "", 1);
+
+	p_map = (People*)mmap(NULL, sizeof(People)*10, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	close(fd);
+	temp = 'a';
+	for (i=0; i<10; i++)
+	{
+		temp += 1;
+		memcpy(&(*(p_map + i)).name, &temp, 2);
+		(*(p_map + i)).age = 20 + i;
+	}
+	printf("Initialize over.\n");
+	sleep(10);
+	munmap(p_map, sizeof(People)*10);
+	printf("Unmap ok.\n");
+
+	return 0;
+}
+
---
+++ test/testRelated.cpp
@@ -0,0 +1,32 @@
+#include <fcntl.h>
+#include <iostream>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <cstdio>
+#include <sys/wait.h>
+#include <cstring>
+
+using namespace std;
+
+class People
+{
+public:
+	string name;
+	int age;
+};
+
+int main(int argc, char *argv[])
+{
+	int fd, i;
+	People *p_map;
+	fd = open(argv[1], O_CREAT|O_RDWR, 00777);
+	p_map = (People*)mmap(NULL, sizeof(People)*10, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	for (i=0;i<10;i++)
+	{
+		//printf("Name: %s age %d;\n", (*(p_map+i)).name.c_str(), (*(p_map+i)).age);	
+		cout << "Name: " << (*(p_map+i)).name << "age: " << (*(p_map+i)).age << endl;
+	}
+	munmap(p_map, sizeof(People)*10);
+}
+
